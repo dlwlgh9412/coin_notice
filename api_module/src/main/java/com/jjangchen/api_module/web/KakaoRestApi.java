3cd52672119406ebc8c793c827f80d4c7dc0152a
Lee Ji HO <dlwlgh9412@gmail.com>
2021-10-05 23:50:58 +0900

    add api_module resources file

--- api_module/src/main/java/com/jjangchen/api_module/web/KakaoRestApi.java
+++ api_module/src/main/java/com/jjangchen/api_module/web/KakaoRestApi.java
@@ -1,20 +1,13 @@
 package com.jjangchen.api_module.web;
 
-import com.jjangchen.api_module.client.kakao.KaKaoApiClient;
-import com.jjangchen.api_module.client.kakao.model.KakaoTokenResponse;
 import com.jjangchen.api_module.service.KakaoService;
 import com.jjangchen.api_module.web.support.ApiRestSupport;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import retrofit2.Response;
 
 import java.io.IOException;
-import java.net.http.HttpResponse;
 
 @Slf4j
 @RequiredArgsConstructor
@@ -27,4 +20,5 @@ public class KakaoRestApi extends ApiRestSupport {
     public ResponseEntity<?> login(@RequestParam("code") String code) throws IOException {
         return response(kakaoService.getToken(code));
     }
+
 }
